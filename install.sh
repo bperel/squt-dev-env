7d6bed32452d5180bbec8ecaee452dc5cd6d5073
Bruno Perel <brunoperel@gmail.com>
2016-03-25 16:23:41 +0100

    Upgrade CLion version Only download CLion if it doesn't exist yet

--- install.sh
+++ install.sh
@@ -1,46 +1,56 @@
 #!/usr/bin/env bash
 
+set -x
+su - vagrant
+
 export DEBIAN_FRONTEND=noninteractive
 
 mariadb_version=mariadb-10.0.21
-clion_version=CLion-144.4199.17
+clion_version=CLion-2016.1
 
+touch /home/vagrant/.bash_profile && \
+(if grep -v -q startx /home/vagrant/.bash_profile; then echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx" >> /home/vagrant/.bash_profile; fi) && \
+\
 # Adding package sources for NodeJS and Java 8. The NodeJS script executes apt-get update as well
 curl -sL https://deb.nodesource.com/setup_4.x | bash - && \
 echo 'oracle-java8-installer shared/accepted-oracle-license-v1-1 boolean true' | debconf-set-selections && \
-printf 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main\n
-deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> webupd8team-java.list && \
+(if [ ! -f /etc/apt/sources.list.d/webupd8team-java.list ]; then \
+printf 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main\n\ndeb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /etc/apt/sources.list.d/webupd8team-java.list; fi) && \
 # Installing NodeJS
 apt-get -y install nodejs && \
 \
 # Installing node-mariasql and dependencies
 apt-get -y install gcc g++ libncurses5-dev bison clang && \
-mkdir -p /home/vagrant/Documents/workspace && cd $_ && \
-(\
-if cd node-mariasql;\
-then git pull;\
-else git clone https://github.com/bperel/node-mariasql.git;\
-fi\
-) && cd node-mariasql && \
-npm install && cd .. && \
-\
-# Installing MariaSQL and dependencies, suppressing the warning issued by dch
-(\
-if cd ${mariadb_version};\
-then git pull origin ${mariadb_version};\
-else git clone -b ${mariadb_version} --depth 1 https://github.com/mariadb/server ${mariadb_version};\
-fi\
-) && cd ${mariadb_version} && \
-(\
-if [ ! -f debian/autobake-deb-force-distribution.sh ];\
-then sed 's/^dch/dch --force-distribution/' debian/autobake-deb.sh > debian/autobake-deb-force-distribution.sh;\
-fi\
+(mkdir -p /home/vagrant/Documents/workspace && cd $_ && \
+ (\
+  if [ -d node-mariasql ]; \
+   then (cd node-mariasql && git pull); \
+   else git clone https://github.com/bperel/node-mariasql.git;\
+  fi\
+ ) && \
+ (cd node-mariasql && npm install) && \
+ \
+ # Installing MariaSQL's dependencies
+ apt-get -y install cmake zlib1g-dev libjemalloc-dev chrpath dh-apparmor dpatch libaio-dev libboost-dev libjudy-dev libpam0g-dev libreadline-gplv2-dev libssl-dev libwrap0-dev gawk hardening-wrapper devscripts && \
+ # Installing MariaSQL and dependencies, suppressing the warning issued by dch
+ (\
+  if [ -d ${mariadb_version} ]; \
+   then (cd ${mariadb_version} && git pull origin ${mariadb_version}); \
+   else git clone -b ${mariadb_version} --depth 1 https://github.com/mariadb/server ${mariadb_version};\
+  fi\
+ ) && \
+ (cd ${mariadb_version} && \
+  (\
+   if [ ! -f debian/autobake-deb-force-distribution.sh ];\
+    then sed 's/^dch/dch --force-distribution/' debian/autobake-deb.sh > debian/autobake-deb-force-distribution.sh;\
+   fi\
+  ) && \
+  chmod +x debian/autobake-deb-force-distribution.sh && \
+  debian/autobake-deb-force-distribution.sh
+ )
 ) && \
-chmod +x debian/autobake-deb-force-distribution.sh && \
-apt-get -y install cmake zlib1g-dev libjemalloc-dev chrpath dh-apparmor dpatch libaio-dev libboost-dev libjudy-dev libpam0g-dev libreadline-gplv2-dev libssl-dev libwrap0-dev gawk hardening-wrapper devscripts && \
-debian/autobake-deb-force-distribution.sh && \
 \
-# Installing the desktop environment
+# Installing desktop environment
 apt-get -y install xfce4 gedit && \
 \
 # Installing Java 8 and other CLion dependencies
@@ -50,5 +60,7 @@ apt-get -y --force-yes install oracle-java8-installer && \
 # Cleaning up
 apt-get -y autoremove && \
 \
-# Downloading CLion
-wget http://download.jetbrains.com/cpp/${clion_version}.tar.gz && tar -xvzf ${clion_version}.tar.gz && rm ${clion_version}.tar.gz \
+# Downloading and extracting CLion
+if [ ! -d ${clion_version} ]; \
+ then wget http://download.jetbrains.com/cpp/${clion_version}.tar.gz && tar -xvzf ${clion_version}.tar.gz && rm ${clion_version}.tar.gz; \
+ fi
